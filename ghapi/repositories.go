96cdd5cb4870f0fc915ec1ed941a7ed6588f8cd8
Piotr Persona <persona.piotr@gmail.com>
2019-11-24 18:08:32 +0100

    Paralellize fetching repository data
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- ghapi/repositories.go
+++ ghapi/repositories.go
@@ -2,6 +2,7 @@ package ghapi
 
 import (
 	"context"
+	"sync"
 
 	"github.com/piotrpersona/gg/model"
 
@@ -13,15 +14,28 @@ import (
 // []model.Repository.
 func FetchRepositories(githubClient *github.Client, limit int) (repositories []neo.Resource, err error) {
 	ctx := context.Background()
-	options := &github.RepositoryListAllOptions{Since: 1}
-	// Read concurrently from GH API
-	githubRepositories, _, err := githubClient.Repositories.ListAll(ctx, options)
-	if err != nil {
-		return
+	var wg sync.WaitGroup
+	wg.Add(limit)
+	githubRepositoriesChannel := make(chan []*github.Repository, limit)
+	for i := 1; i <= limit; i++ {
+		since := int64(i)
+		go func(wg *sync.WaitGroup, since int64, grc chan []*github.Repository) {
+			defer wg.Done()
+			options := &github.RepositoryListAllOptions{Since: since}
+			githubRepositories, _, err := githubClient.Repositories.ListAll(ctx, options)
+			if err != nil {
+				return
+			}
+			githubRepositoriesChannel <- githubRepositories
+		}(&wg, since, githubRepositoriesChannel)
 	}
-	for _, githubRepository := range githubRepositories {
-		repository := model.CreateRepository(githubRepository)
-		repositories = append(repositories, repository)
+	wg.Wait()
+	close(githubRepositoriesChannel)
+	for githubRepositories := range githubRepositoriesChannel {
+		for _, githubRepository := range githubRepositories {
+			repository := model.CreateRepository(githubRepository)
+			repositories = append(repositories, repository)
+		}
 	}
 	return repositories, nil
 }
