e5c14bdb5106e7dd0b5fb4de37f33027ee03da24
Piotr Persona <persona.piotr@gmail.com>
2019-11-24 17:32:13 +0100

    Fix #10 Parallelize
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,5 @@
 FROM golang:1.12.1-alpine3.9 AS stage-build
 
-# Microbadger: https://microbadger.com/labels
-ARG VCS_REF
-LABEL org.label-schema.vcs-ref=$VCS_REF \
-        org.label-schema.vcs-url="https://github.com/piotrpersona/gg"
-
-
 RUN apk add --no-cache ca-certificates cmake make g++ openssl-dev git curl pkgconfig
 
 RUN git clone -b 1.7 https://github.com/neo4j-drivers/seabolt.git /seabolt
@@ -40,5 +34,10 @@ RUN apk update && apk add ca-certificates
 
 COPY --from=stage-build \
     /go/bin/gg /usr/local/bin/gg
+
+# Microbadger: https://microbadger.com/labels
+ARG VCS_REF
+LABEL org.label-schema.vcs-ref=$VCS_REF \
+        org.label-schema.vcs-url="https://github.com/piotrpersona/gg"
 
 ENTRYPOINT [ "/usr/local/bin/gg" ]
--- app/app.go
+++ app/app.go
@@ -2,13 +2,37 @@ package app
 
 import (
 	"os"
+	"sync"
 
+	"github.com/google/go-github/github"
 	"github.com/piotrpersona/gg/ghapi"
 	"github.com/piotrpersona/gg/model"
 	"github.com/piotrpersona/gg/neo"
 
 	log "github.com/sirupsen/logrus"
 )
+
+func fetchResources(
+	neocfg neo.Config,
+	githubClient *github.Client,
+	repoResources []ghapi.RepoResource,
+	repoModel model.Repository,
+) {
+	var resourcesWg sync.WaitGroup
+	numberOfResourcesTasks := len(repoResources)
+	resourcesWg.Add(numberOfResourcesTasks)
+	for _, repoResource := range repoResources {
+		go func(wg *sync.WaitGroup, repoResource ghapi.RepoResource) {
+			defer wg.Done()
+			resources, err := repoResource.Fetch(githubClient, repoModel)
+			if err != nil {
+				log.Error(err)
+			}
+			neo.Neoize(neocfg, resources...)
+		}(&resourcesWg, repoResource)
+	}
+	resourcesWg.Wait()
+}
 
 // Run will run application with provided application config
 func Run(appConfig ApplicationConfig) {
@@ -28,14 +52,20 @@ func Run(appConfig ApplicationConfig) {
 		ghapi.Issues{},
 	}
 
+	var repoWg sync.WaitGroup
+	numberOfRepoTasks := len(repositories)
+	repoWg.Add(numberOfRepoTasks)
 	for _, repository := range repositories {
-		repoModel := repository.(model.Repository)
-		for _, repoResource := range repoResources {
-			resources, err := repoResource.Fetch(githubClient, repoModel)
-			if err != nil {
-				log.Error(err)
-			}
-			neo.Neoize(neoconfig, resources...)
-		}
+		go func(repoWg *sync.WaitGroup, repository neo.Resource) {
+			defer repoWg.Done()
+			repoModel := repository.(model.Repository)
+			fetchResources(
+				neoconfig,
+				githubClient,
+				repoResources,
+				repoModel,
+			)
+		}(&repoWg, repository)
 	}
+	repoWg.Wait()
 }
